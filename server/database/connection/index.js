bc634c349b4b5a32800b044d88250a4975ff67ec
modrayyes <mod.rayyes@gmail.com>
2021-10-02 18:37:25 +0300

    database conniection and build

---
+++ server/database/connection/index.js
@@ -0,0 +1,10 @@
+require("env2")(".env");
+const { Pool } = require("pg");
+
+const connectionString = process.env.DB_URL;
+
+const pool = new Pool({
+  connectionString,
+  max: process.env.DB_MAX_CONNECTIONS || 2,
+  ssl: { rejectUnauthorized: false },
+});
