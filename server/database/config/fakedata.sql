cf2a89049844efe5c191a975274e2894b898a950
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-03 17:14:38 +0300

    insert fake data #30

---
+++ server/database/config/fakedata.sql
@@ -0,0 +1,148 @@
+-- sofas
+insert into product (name, image, price, category) values 
+('Timber Charme Tan Sofa',  'https://cdn-images.article.com/products/SKU2128/2890x1500/image58175.jpg?fit=max&w=425&q=80&fm=webp', 1899, 'Sofas');
+insert into product (name, image, price, category) values 
+('Abisko Quartz White Sofa', 'https://cdn-images.article.com/products/SKU11884/2890x1500/image46333.jpg?fit=max&w=425&q=80&fm=webp', 1199, 'Sofas');
+insert into product (name, image, price, category) values 
+('Sven Neptune Blue Sofa', 'https://cdn-images.article.com/products/SKU15197/2890x1500/image64200.jpg?fit=max&w=425&q=80&fm=webp', 1099, 'Sofas');
+insert into product (name, image, price, category) values 
+('Abisko Aurora Blue Left Sectional', 'https://cdn-images.article.com/products/SKU14216/2890x1500/image63783.jpg?fit=max&w=425&q=80&fm=webp', 1699, 'Sofas');
+insert into product (name, image, price, category) values 
+('Ceni Aquarius Aqua Sofa', 'https://cdn-images.article.com/products/SKU419C/2890x1500/image60521.jpg?fit=max&w=425&q=80&fm=webp', 999, 'Sofas');
+insert into product (name, image, price, category) values 
+('Gabriola Ivory Bouclé Loveseat', 'https://cdn-images.article.com/products/SKU12859/2890x1500/image46825.jpg?fit=max&w=425&q=80&fm=webp', 699, 'Sofas');
+insert into product (name, image, price, category) values 
+('Lupra Hibiscus Pink Daybed', 'https://cdn-images.article.com/products/SKU14224/2890x1500/image54281.jpg?fit=max&w=425&q=80&fm=webp', 699, 'Sofas');
+insert into product (name, image, price, category) values 
+('Sven Cascadia Blue Sofa', 'https://cdn-images.article.com/products/SKU338D/2890x1500/image57264.jpg?fit=max&w=425&q=80&fm=webp', 1399, 'Sofas');
+insert into product (name, image, price, category) values 
+('Sven Charme Chocolat 72" Sofa', 'https://cdn-images.article.com/products/SKU416D/2890x1500/image57412.jpg?fit=max&w=425&q=80&fm=webp', 1799, 'Sofas');
+insert into product (name, image, price, category) values 
+('Sitka Boreal Gray Sofa', 'https://cdn-images.article.com/products/SKU330F/2890x1500/image50523.jpg?fit=max&w=425&q=80&fm=webp', 1599, 'Sofas');
+insert into product (name, image, price, category) values 
+('Timber Charme Tan Corner Sectional', 'https://cdn-images.article.com/products/SKU3672/2890x1500/image46594.jpg?fit=max&w=425&q=80&fm=webp', 3899, 'Sofas');
+insert into product (name, image, price, category) values 
+('Ceni Aquarius Aqua Loveseat', 'https://cdn-images.article.com/products/SKU420A/2890x1500/image60674.jpg?fit=max&w=425&q=80&fm=webp', 799, 'Sofas');
+insert into product (name, image, price, category) values 
+('Burrard Seasalt Gray Sofa', 'https://cdn-images.article.com/products/SKU431A/2890x1500/image22841.jpg?fit=max&w=425&q=80&fm=webp', 1299, 'Sofas');
+insert into product (name, image, price, category) values 
+('Sven Neptune Blue Right Sectional Sofa', 'https://cdn-images.article.com/products/SKU15220/2890x1500/image64219.jpg?fit=max&w=425&q=80&fm=webp', 2099, 'Sofas');
+insert into product (name, image, price, category) values 
+('Cigar Rawhide Brown Sofa', 'https://cdn-images.article.com/products/SKU2307/2890x1500/image58090.jpg?fit=max&w=425&q=80&fm=webp', 2349, 'Sofas');
+insert into product (name, image, price, category) values 
+('Divan Mist Gray Daybed', 'https://cdn-images.article.com/products/SKU12989/2890x1500/image53822.jpg?fit=max&w=425&q=80&fm=webp', 1199, 'Sofas');
+insert into product (name, image, price, category) values 
+('Anton Twilight Blue Sofa', 'https://cdn-images.article.com/products/SKU323C/2890x1500/image24094.jpg?fit=max&w=425&q=80&fm=webp', 1099, 'Sofas');
+insert into product (name, image, price, category) values 
+('Texada Taos Tan Sofa', 'https://cdn-images.article.com/products/SKU12986/2890x1500/image58728.jpg?fit=max&w=425&q=80&fm=webp', 2199, 'Sofas');
+insert into product (name, image, price, category) values 
+('Malsa Soft White Slipcover Sofa', 'https://cdn-images.article.com/products/SKU17559/2890x1500/image70595.jpg?fit=max&w=425&q=80&fm=webp', 1699, 'Sofas');
+insert into product (name, image, price, category) values 
+('Solae Hush Gray / Walnut Left Arm Corner Sectional', 'https://cdn-images.article.com/products/SKU12035/2890x1500/image62989.jpg?fit=max&w=425&q=80&fm=webp', 2949, 'Sofas');
+-- Otomans
+insert into product (name, image, price, category) values 
+('Cilo Ivory Bouclé Ottoman', 'https://cdn-images.article.com/products/SKU13492/2890x1500/image51234.jpg?fit=max&w=425&q=80&fm=webp', 139, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Timpani Charme Tan Ottoman', 'https://cdn-images.article.com/products/SKU376/2890x1500/image58158.jpg?fit=max&w=425&q=80&fm=webp', 699, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Sven Charme Chocolat Ottoman', 'https://cdn-images.article.com/products/SKU12895/2890x1500/image57160.jpg?fit=max&w=425&q=80&fm=webp', 549, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Timber Charme Tan Ottoman', 'https://cdn-images.article.com/products/SKU16790/2890x1500/image70710.jpg?fit=max&w=425&q=80&fm=webp', 599, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Sven Neptune Blue Ottoman', 'https://cdn-images.article.com/products/SKU15243/2890x1500/image64243.jpg?fit=max&w=425&q=80&fm=webp', 299, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Hira Natural Ivory 30" Ottoman', 'https://cdn-images.article.com/products/SKU533/2890x1500/image37473.jpg?fit=max&w=425&q=80&fm=webp', 349, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Abisko Quartz White Ottoman', 'https://cdn-images.article.com/products/SKU15652/2890x1500/image63671.jpg?fit=max&w=425&q=80&fm=webp', 349, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Ceni Aquarius Aqua Ottoman', 'https://cdn-images.article.com/products/SKU421A/2890x1500/image60580.jpg?fit=max&w=425&q=80&fm=webp', 389, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Burrard Seasalt Gray Ottoman', 'https://cdn-images.article.com/products/SKU12675/2890x1500/image45931.jpg?fit=max&w=425&q=80&fm=webp', 299, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Sven Cascadia Blue Ottoman', 'https://cdn-images.article.com/products/SKU12901/2890x1500/image57682.jpg?fit=max&w=425&q=80&fm=webp', 349, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Cilo Oxford Blue Ottoman', 'https://cdn-images.article.com/products/SKU16770/2890x1500/image69875.jpg?fit=max&w=425&q=80&fm=webp', 169, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Tablet Charme Tan Square Ottoman', 'https://cdn-images.article.com/products/SKU508/2890x1500/image61622.jpg?fit=max&w=425&q=80&fm=webp', 1099, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Beta Summit Gray Ottoman', 'https://cdn-images.article.com/products/SKU14406/2890x1500/image55366.jpg?fit=max&w=425&q=80&fm=webp', 549, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Kayra Seaside Blue Ottoman', 'https://cdn-images.article.com/products/SKU16615/2890x1500/image66796.jpg?fit=max&w=425&q=80&fm=webp', 319, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Macca Juniper Green Ottoman', 'https://cdn-images.article.com/products/SKU13287/2890x1500/image60291.jpg?fit=max&w=425&q=80&fm=webp', 399, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Otio Black Leather Walnut Ottoman', 'https://cdn-images.article.com/products/SKU12588/2890x1500/image56631.jpg?fit=max&w=425&q=80&fm=webp', 269, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Maribo Stately Gray 32" Storage Ottoman', 'https://cdn-images.article.com/products/SKU16777/2890x1500/image68797.jpg?fit=max&w=425&q=80&fm=webp', 249, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Reni Freckle Gray Ottoman', 'https://cdn-images.article.com/products/SKU13312/2890x1500/image49826.jpg?fit=max&w=425&q=80&fm=webp', 249, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Lappi Serene Gray Ottoman', 'https://cdn-images.article.com/products/SKU14578/2890x1500/image54573.jpg?fit=max&w=425&q=80&fm=webp', 399, 'Ottomans');
+insert into product (name, image, price, category) values 
+('Regis Yarrow Gold Ottoman', 'https://cdn-images.article.com/products/SKU17429/2890x1500/image70215.jpg?fit=max&w=425&q=80&fm=webp', 299, 'Ottomans');
+-- Tables
+insert into product (name, image, price, category) values 
+('Amoeba Wild Walnut End Table', 'https://cdn-images.article.com/products/SKU360/2890x1500/image56029.jpg?fit=max&w=425&q=80&fm=webp', 199, 'Tables');
+insert into product (name, image, price, category) values 
+('Halden Alpine White 16" Round Side Table', 'https://cdn-images.article.com/products/SKU15495/2890x1500/image61804.jpg?fit=max&w=425&q=80&fm=webp', 129, 'Tables');
+insert into product (name, image, price, category) values 
+('Mara Walnut Side Table', 'https://cdn-images.article.com/products/SKU429/2890x1500/image73492.jpg?fit=max&w=425&q=80&fm=webp', 199, 'Tables');
+insert into product (name, image, price, category) values 
+('Vitri Walnut Coffee Table', 'https://cdn-images.article.com/products/SKU3706/2890x1500/image56196.jpg?fit=max&w=425&q=80&fm=webp', 329, 'Tables');
+insert into product (name, image, price, category) values 
+('Halden Dark Charcoal 28" Round Side Table', 'https://cdn-images.article.com/products/SKU13611/2890x1500/image50817.jpg?fit=max&w=425&q=80&fm=webp', 199, 'Tables');
+insert into product (name, image, price, category) values 
+('Calova Side Table', 'https://cdn-images.article.com/products/SKU16278/2890x1500/image63927.jpg?fit=max&w=425&q=80&fm=webp', 149, 'tables');
+insert into product (name, image, price, category) values 
+('Atica Dining Table for 8', 'https://cdn-images.article.com/products/SKU12540/2890x1500/image44916.jpg?fit=max&w=425&q=80&fm=webp', 799, 'Tables');
+insert into product (name, image, price, category) values 
+('Silicus Navy Side Table', 'https://cdn-images.article.com/products/SKU15267/2890x1500/image61219.jpg?fit=max&w=425&q=80&fm=webp', 119, 'Tables');
+insert into product (name, image, price, category) values 
+('Amoeba Wild Walnut Coffee Table', 'https://cdn-images.article.com/products/SKU359/2890x1500/image56088.jpg?fit=max&w=425&q=80&fm=webp', 349, 'Tables');
+insert into product (name, image, price, category) values 
+('Tromso Brass Coffee Table', 'https://cdn-images.article.com/products/SKU13863/2890x1500/image52677.jpg?fit=max&w=425&q=80&fm=webp', 499, 'Tables');
+insert into product (name, image, price, category) values 
+('Equa Brushed Brass Side Table', 'https://cdn-images.article.com/products/SKU11592/2890x1500/image39734.jpg?fit=max&w=425&q=80&fm=webp', 259, 'Tables');
+insert into product (name, image, price, category) values 
+('Ventu Light Oak Dining Table for 8', 'https://cdn-images.article.com/products/SKU11382/2890x1500/image46467.jpg?fit=max&w=425&q=80&fm=webp', 899, 'Tables');
+insert into product (name, image, price, category) values 
+('Ventu Light Oak Round Dining Table', 'https://cdn-images.article.com/products/SKU11383/2890x1500/image48045.jpg?fit=max&w=425&q=80&fm=webp', 699, 'Tables');
+insert into product (name, image, price, category) values 
+('Calliope Black Dining Table', 'https://cdn-images.article.com/products/SKU13559/2890x1500/image52529.jpg?fit=max&w=425&q=80&fm=webp', 549, 'Tables');
+insert into product (name, image, price, category) values 
+('Tima Sand Brown Coffee Table', 'https://cdn-images.article.com/products/SKU16279/2890x1500/image63455.jpg?fit=max&w=425&q=80&fm=webp', 199, 'Tables');
+insert into product (name, image, price, category) values 
+('Lagora Washed Oak Dining Table for 4', 'https://cdn-images.article.com/products/SKU15577/2890x1500/image65032.jpg?fit=max&w=425&q=80&fm=webp', 449, 'Tables');
+insert into product (name, image, price, category) values 
+('Atra Concrete Round Coffee Table', 'https://cdn-images.article.com/products/SKU12538/2890x1500/image65952.jpg?fit=max&w=1050&q=80&fm=webp', 249, 'Tables');
+insert into product (name, image, price, category) values 
+('Atra Concrete Round Cafe Table', 'https://cdn-images.article.com/products/SKU12430/2890x1500/image43978.jpg?fit=max&w=1050&q=80&fm=webp', 399, 'Tables');
+insert into product (name, image, price, category) values 
+('Amoeba Wild Walnut 35" Wide Coffee Table', 'https://cdn-images.article.com/products/SKU388/2890x1500/image56125.jpg?fit=max&w=1050&q=80&fm=webp', 249, 'Tables');
+insert into product (name, image, price, category) values 
+('Lenia Walnut Square Coffee Table', 'https://cdn-images.article.com/products/SKU13604/2890x1500/image62348.jpg?fit=max&w=1050&q=80&fm=webp', 349, 'Tables');
+-- Pillows
+insert into product (name, image, price, category) values 
+('Gabriola Ivory Bouclé Pillow Set', 'https://cdn-images.article.com/products/SKU12793/2890x1500/image46665.jpg?fit=max&w=425&q=80&fm=webp', 69, 'Pillows');
+insert into product (name, image, price, category) values 
+('Lanna Ivory Sheepskin Seat Pad Set', 'https://cdn-images.article.com/products/SKU11139/2890x1500/image35571.jpg?fit=max&w=425&q=80&fm=webp', 79, 'Pillows');
+insert into product (name, image, price, category) values 
+('Stitch Aqua Pillow', 'https://cdn-images.article.com/products/SKU13216/2890x1500/image48157.jpg?fit=max&w=425&q=80&fm=webp', 49, 'Pillows');
+insert into product (name, image, price, category) values 
+('Criss Natural Ivory Pillow', 'https://cdn-images.article.com/products/SKU13217/2890x1500/image48078.jpg?fit=max&w=425&q=80&fm=webp', 49, 'Pillows');
+insert into product (name, image, price, category) values 
+('Lucca Cascadia Blue Pillow Set', 'https://cdn-images.article.com/products/SKU3031/2890x1500/image59480.jpg?fit=max&w=425&q=80&fm=webp', 99, 'Pillows');
+insert into product (name, image, price, category) values 
+('Lanna Ivory Sheepskin Pillow', 'https://cdn-images.article.com/products/SKU2181/2890x1500/image66832.jpg?fit=max&w=425&q=80&fm=webp', 59, 'Pillows');
+insert into product (name, image, price, category) values 
+('Lanna Pink Round Sheepskin Pillow', 'https://cdn-images.article.com/products/SKU13222/2890x1500/image48589.jpg?fit=max&w=425&q=80&fm=webp', 49, 'Pillows');
+insert into product (name, image, price, category) values 
+('Sanka Sandstorm Pillow', 'https://cdn-images.article.com/products/SKU16750/2890x1500/image66553.jpg?fit=max&w=425&q=80&fm=webp', 69, 'Pillows');
+insert into product (name, image, price, category) values 
+('Jema Oxford Navy Pillow', 'https://cdn-images.article.com/products/SKU17132/2890x1500/image68319.jpg?fit=max&w=425&q=80&fm=webp', 49, 'Pillows');
+insert into product (name, image, price, category) values 
+('Ren Large Pillow', 'https://cdn-images.article.com/products/SKU13230/2890x1500/image48106.jpg?fit=max&w=425&q=80&fm=webp', 59, 'Pillows');
+insert into product (name, image, price, category) values 
+('Lucca Blush Pink Pillow Set', 'https://cdn-images.article.com/products/SKU3030/2890x1500/image59441.jpg?fit=max&w=425&q=80&fm=webp', 99, 'Pillows');
+insert into product (name, image, price, category) values 
+('Stitch Pink Pillow', 'https://cdn-images.article.com/products/SKU13214/2890x1500/image48178.jpg?fit=max&w=425&q=80&fm=webp', 49, 'Pillows');
