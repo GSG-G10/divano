8ad11c6d268c1d2d8333b4595bca2e58f3ff9cea
modrayyes <mod.rayyes@gmail.com>
2021-10-03 18:03:51 +0300

    edit query

--- server/database/queries/products/index.js
+++ server/database/queries/products/index.js
@@ -1,10 +1,10 @@
 const connection = require('../../connection');
 
-const getProductsQuery = ({ id }) => connection.query(
-  'SELECT * FROM products WHERE id=$1', [id]);
+const getProductsQuery = () => connection.query(
+  'SELECT * FROM products WHERE id=$1');
 
 module.exports = {
-    getProductsQuery,
+    getProductsQuery
 }
 
 
