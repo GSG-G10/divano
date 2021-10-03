c568439e4e4a5c4332cad7267b1fbe65ddf481c5
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-03 21:54:41 +0300

    sent json msg to handle the request in the front #39

--- server/controllers/logout.js
+++ server/controllers/logout.js
@@ -1,5 +1,6 @@
 const logout = (req, res) => {
-  res.clearCookie('token', 'username');
+  res.clearCookie('token', 'username')
+    .json({ msg: 'logged out succefully' });
 };
 
 module.exports = logout;
