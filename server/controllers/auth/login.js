34f6765798a4a3fbaf8b94ec1134fe453a948cf9
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-04 20:53:26 +0300

    resolve nested promises by turn the type to async-await functions

--- server/controllers/auth/login.js
+++ server/controllers/auth/login.js
@@ -1,35 +1,30 @@
 const bcrypt = require('bcryptjs');
-const { createSession } = require('./index');
 const { getUserQuery } = require('../../database/queries');
 const { loginSchema } = require('../../utils/validations');
 
-const login = (req, res, next) => {
-  const { password, email } = req.body;
-  loginSchema.validateAsync(req.body)
-    .then(() => {
-      getUserQuery(email)
-        .then((row) => {
-          // check if username exists
-          if (!row.length) {
-            res.status(401).json({ message: 'invalid email or password' });
-          } else {
-            // check password
-            bcrypt.compare(password, row[0].password).then((data) => {
-              if (data) { // send cookies and response
-                res.cookie('username', row[0].useername);
-                createSession(email, row[0].id)
-                  .then((token) => {
-                    res.cookie('token', token, { httpOnly: true, secure: true });
-                    res.json({ message: 'logged in successfully' });
-                  });
-              } else res.status(401).json({ message: 'invalid email or password' });
-            })
-              .catch((err) => next(err));
-          }
-        }).catch((err) => next(err));
-    }).catch((err) => {
+const login = async (req, res, next) => {
+  // eslint-disable-next-line global-require
+  const { createSession } = require('./index'); // there is a problem with requiring this function
+  try {
+    const { password, email } = req.body;
+    await loginSchema.validateAsync(req.body).catch((err) => {
       res.status(422).json({ message: err.message });
     });
+    const { rows } = await getUserQuery(email);
+    if (!rows.length) {
+      return res.status(401).json({ message: 'invalid email or password' });
+    }
+    const compared = await bcrypt.compare(password, rows[0].password);
+    if (!compared) {
+      return res.status(401).json({ message: 'invalid email or password' });
+    }
+    const token = await createSession(email, rows[0].id);
+    res.cookie('token', token);
+    res.cookie('username', rows[0].username);
+    return res.json({ message: 'logged in successfully' });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = login;
