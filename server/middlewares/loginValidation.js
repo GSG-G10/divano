d0e6671face28405d829a48f9d302f9d67725f74
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-04 11:17:41 +0300

    refactor validation middleware and add schema to utils

--- server/middlewares/loginValidation.js
+++ server/middlewares/loginValidation.js
@@ -1,11 +1,7 @@
-const joi = require('joi');
+const { loginSchema } = require('../utils/validations');
 // login input Validation
 const loginValidation = (req, res, next) => {
-  const schema = joi.object({
-    email: joi.string().email().required(),
-    password: joi.string().alphanum().min(6).required(),
-  });
-  schema.validateAsync(req.body)
+  loginSchema.validateAsync(req.body)
     .then(() => {
       next();
     }).catch((err) => {
