1ad7220c9a2b983147514503f342d2f583975398
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-05 16:00:17 +0300

    transfer the navbar to a seprate component, clean code #3

--- client/src/components/mainpage/MainHeader.js
+++ client/src/components/mainpage/MainHeader.js
@@ -1,69 +1,10 @@
 import "./Style.css";
-import { Box, AppBar, Toolbar } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Link } from "react-router-dom";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-// import { useEffect, useState } from "react";
-
-const useStyles = createTheme({
-  palette: {
-    primary: {
-      //darkBlue
-      main: "#04263F",
-    },
-    secondary: {
-      //Brown
-      main: "#99580B",
-    },
-  },
-});
-
+import { Box } from "@mui/material";
+import NavBar from './NavBar'
 function MainHeader({username}) {
   return (
     <>
-      <ThemeProvider theme={useStyles}>
-        <AppBar color="transparent">
-          <Toolbar>
-            <Link to="/" className="logo">
-              Divano
-            </Link>
-            <ul className="navList">
-              <li>
-                <Link to="/" className="listItem active">
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/products" className="listItem">
-                  Furniture
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="listItem">
-                  About us
-                </Link>
-              </li>
-              <li>
-                <Link to="/" className="listItem">
-                  Contact us
-                </Link>
-              </li>
-            </ul>
-            <Toolbar className="navBtns">
-              <button className="loginBtn">Log In</button>
-              <div className="userContainer hidden">
-                <AccountCircleIcon color="secondary"/>
-                <span className="userName">{username}</span>
-              </div> 
-              <div className="cartContainer">
-                <ShoppingCartIcon className="cartIcon" />
-                <span className="counter">0</span>
-              </div>
-            </Toolbar>
-          </Toolbar>
-        </AppBar>
-      </ThemeProvider>
+      <NavBar />
       <Box className="banner">
         <Box className="welcome">
           Furniture that <br /> Everyone <br /> loves
