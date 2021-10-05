e3ea879ae1b6e26454dcce6e931dd8b95946ecd5
mohammed Rayyes <mod.rayyes@gmail.com>
2021-10-05 14:36:29 +0300

    exports and requires edits

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,14 @@
 const { getCart } = require('./pages');
 const productDetails = require('./productDetails');
-const { login, logout } = require('./auth');
+const { login, logout , signUp } = require('./auth');
+const { error404, serverError } = require('./errors');
 
 module.exports = {
   login,
   logout,
   getCart,
   productDetails,
+  error404,
+  serverError,
+  signUp,
 };
