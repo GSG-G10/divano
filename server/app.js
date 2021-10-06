023d826c915886f988e058afa2c46f0346879202
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-10-06 03:04:19 +0300

    build heroku app Relates #61

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+const { join } = require('path');
 require('env2')('.env');
 const express = require('express');
 const cookieParser = require('cookie-parser');
@@ -10,7 +11,11 @@ app.disable('x-powered-by');
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'client', 'build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 app.use('/api/v1', router);
 
 module.exports = app;
