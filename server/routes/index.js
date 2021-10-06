7ad9b43f2efe1395bbf434e2db82ff811fd3c185
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-10-06 23:42:46 +0300

    add the add to cart router Relates #14

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,16 +1,15 @@
 const router = require('express').Router();
-const signupValidation = require('../middlewares');
-const { signUp } = require('../controllers/auth/signup');
 const {
-  logout, getCart, login, productDetails,error404, serverError ,signUp,
+  logout, getCart, login, productDetails, error404, serverError, signUp,
 } = require('../controllers');
-const { authentication } = require('../middlewares');
+const { authentication, signupValid } = require('../middlewares');
 
-router.post('/signup', signupValidation, signUp);
+router.post('/signup', signupValid, signUp);
 router.post('/login', login);
 router.get('/product/:id', productDetails);
 router.get('/logout', logout);
 router.get('/cart', authentication, getCart);
+router.post('/cart', authentication, getCart);
 router.use(error404);
 router.use(serverError);
 
