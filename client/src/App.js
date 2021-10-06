7bd19338245d1839c3d895282237c523efe7fa7b
NadiaJalalTamimi <nadia.tamimi74@gmail.com>
2021-10-06 17:05:27 +0300

    add  error pages to the router Relates #59

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 /* eslint-disable react/react-in-jsx-scope */
-import './App.css';
+import React,{useState} from "react";
+import {BrowserRouter as Router , Route ,Switch} from "react-router-dom"
+import "antd/dist/antd.css";
+import Signup from "./commponants/Signup";
+import PageNotFound from "./commponants/errors/PageNotFound";
+import Servererror from "./commponants/errors/Servererror";
 import MainHeader from './components/mainpage/MainHeader';
+import "./App.css";
 
 function App() {
+  const [active,setActive] = useState("");
   return (
-    <div className="App">
+    <div>
       <MainHeader />
+      <Router>
+        <Switch>
+        <Route path="/500" ><Servererror /></Route>
+        <Route path="/signup"><Signup active={active} setActive={setActive} />
+        <button onClick={() => setActive("active")}>Hello Ahmad</button>
+        </Route>
+  
+      <Route ><PageNotFound/></Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
