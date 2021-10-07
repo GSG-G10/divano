bd6009b1feb408aae5cbe298297c096e139795a3
mohammed Rayyes <mod.rayyes@gmail.com>
2021-10-07 12:37:29 +0300

    formating

--- client/src/pages/cart/cart.js
+++ client/src/pages/cart/cart.js
@@ -1,33 +1,33 @@
 // import './style.css';
-import React, {useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import NavBar from  '../../components/common/NavBar/NavBar'
+import NavBar from '../../components/common/NavBar/NavBar'
 import CustomizedTables from '../../components/cartTable/cartTable'
 
 
 
 
 function Cart() {
-  function totalCost () {
+  function totalCost() {
     let totalCost = 0;
-    cart.forEach (ele => (totalCost = totalCost + ele.price * ele.quantity));
+    cart.forEach(ele => (totalCost = totalCost + ele.price * ele.quantity));
     return totalCost;
   }
 
-  const [cart, setCart] = useState ();
-  useEffect (() => {
+  const [cart, setCart] = useState();
+  useEffect(() => {
     axios
-      .get ('/userProducts')
-      .then (response => response.data)
-      .then (data => setCart (data))
-      .catch (err => console.log (err));
+      .get('/userProducts')
+      .then(response => response.data)
+      .then(data => setCart(data))
+      .catch(err => console.log(err));
   }, []);
 
   return (
-      <div>
-          <NavBar/>
-        <CustomizedTables/>
-      </div>
+    <div>
+      <NavBar />
+      <CustomizedTables />
+    </div>
   )
 
 }
