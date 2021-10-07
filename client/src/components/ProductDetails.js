a2a8111a1d2e77a1e18894455b359b40678a3676
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-07 13:25:34 +0300

    fetch the data #12

--- client/src/components/ProductDetails.js
+++ client/src/components/ProductDetails.js
@@ -1,85 +1,92 @@
-import { Card, CardMedia, Typography, CardContent, CardActions, Button, CircularProgress } from "@mui/material";
+import {
+  Card, CardMedia, Typography, CardContent, CardActions, Button, CircularProgress,
+} from '@mui/material';
+import { useState, useEffect } from 'react';
 import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
 import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useState, useEffect } from "react";
-import getProduct from "../utils/getProducts";
-// import axios from 'axios';
+import getProduct from '../utils/getProducts';
 
-function ProductDetails({setNavBackground}) {
-  // setNavBackground('primary');
+function ProductDetails({ setNavBackground }) {
   const [product, setProduct] = useState([]);
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true)
-
-  // const anything = async () => {
-  //   try{
-  //     const response = await axios('/api/v1/products/3',{
-  //       method: 'GET',
-  //     })
-  //     console.log('res', response)
-  //     setProduct(response);
-  //     console.log('product', product)
-  //   }catch (err){
-  //     setError()
-  //   }
-  // }
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
+    setNavBackground('primary');
+    const controller = new AbortController();
+
     getProduct()
-    .then(res => {
-      if(res){
-        const {data} = res;
-        setProduct(data);
-        console.log('res', res)
-        console.log('product', product)
-      }
-    }).catch(err => setError(err))
-    .finally(() => setLoading(false));
-    
-  }, [])
+      .then((res) => {
+        if (res) {
+          const { data } = res;
+          setProduct(data);
+        }
+      }).catch((err) => setError(err))
+      .finally(() => setLoading(false));
 
-  // if (loading) {
-  //   return (
-  //       <CircularProgress />
-  //   );
-  // }
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
-  // if (!(product && product.length)) {
-  //   return null;
-  // }
+  if (loading) {
+    return (
+      <CircularProgress />
+    );
+  }
+
+  if (!(product && product.length)) {
+    return null;
+  }
 
   return (
     <>
-    <Card sx={{ display:'flex', paddingTop:'3rem', margin:'auto', marginTop:'5rem' }}>
-        <CardMedia
-        component="img"
-        sx={{ width: 900 }}
-        image="https://cdn-images.article.com/products/SKU25A/2890x1500/image60499.jpg?fit=max&w=1050&q=80&fm=webp"
-        alt="product"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div" color="primary">
-          anything
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{display: "flex", paddingTop: '1rem', paddingBottom: '1rem'}}>
-          <DirectionsCarIcon color="primary" sx={{paddingRight: '.5rem'}}/> Delivered to Los Angeles, CA : Oct 18th - 29th
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{display: "flex", paddingTop: '1rem', paddingBottom: '1rem'}}>
-          <CheckCircleOutlineIcon color="primary" sx={{paddingRight: '.5rem'}}/> In Stock
-        </Typography>
-        <CardActions sx={{display: "flex", justifyContent: 'space-between', paddingTop: '1rem', paddingBottom: '1rem'}}>
-        <Typography variant="body6" color="secondary" sx={{fontWeight : '700'}}>
-          699$
-        </Typography>
-          <Button variant="contained" color="primary">
-            <ShoppingCartIcon /> ADD TO CART
-          </Button>
-        </CardActions>
-      </CardContent>
-    </Card>
+      {product.map((pro) => (
+        <Card sx={{
+          display: 'flex', paddingTop: '3rem', margin: 'auto', marginTop: '7rem',
+        }}
+        >
+          <CardMedia
+            component="img"
+            sx={{ width: 900 }}
+            image={pro.image}
+            alt="product"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div" color="primary">
+              {pro.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', paddingTop: '1rem', paddingBottom: '1rem' }}>
+              <DirectionsCarIcon color="primary" sx={{ paddingRight: '.5rem' }} />
+              {' '}
+              Delivered to Los Angeles, CA : Oct 18th - 29th
+            </Typography>
+            <Typography variant="body2" color="text.secondary" sx={{ display: 'flex', paddingTop: '1rem', paddingBottom: '1rem' }}>
+              <CheckCircleOutlineIcon color="primary" sx={{ paddingRight: '.5rem' }} />
+              {' '}
+              In Stock
+            </Typography>
+            <CardActions sx={{
+              display: 'flex', justifyContent: 'space-between', paddingTop: '1rem', paddingBottom: '1rem',
+            }}
+            >
+              <Typography variant="body6" color="secondary" sx={{ fontWeight: '700' }}>
+                {pro.price}
+                $
+              </Typography>
+              <Button variant="contained" color="primary">
+                <ShoppingCartIcon />
+                {' '}
+                ADD TO CART
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      ))}
+
     </>
-    
+
   );
 }
 
--- client/src/utils/getProducts.js
+++ client/src/utils/getProducts.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
+import Axios from 'axios';
 
 const getProduct = async () => {
   const url = '/api/v1/products/4';
-  return axios.get(url)
-  .then((res) => res)
-  .catch((err) => {
-    throw new Error(`Error in fetching products ${err}`);
-  });
-}
+  return Axios.get(url)
+    .then((response) => response)
+    .catch((err) => {
+      throw new Error(`Error in fetching products ${err}`);
+    });
+};
 
 export default getProduct;
