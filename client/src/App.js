7384be20734fc0a3683fdaeb1d42f8f031bad66a
asmaaamin <eng.asmaa.a.an@gmail.com>
2021-10-07 13:26:02 +0300

    fix eslint issues #12

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,43 @@
 /* eslint-disable react/react-in-jsx-scope */
-import { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import "./App.css";
-import NavBar from "./components/common/NavBar/NavBar";
-import MainHeader from "./components/mainpage/MainHeader";
-import ProductDetails from "./components/ProductDetails";
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './App.css';
+import NavBar from './components/common/NavBar/NavBar';
+import MainHeader from './components/mainpage/MainHeader';
+import ProductDetails from './components/ProductDetails';
 
 const theme = createTheme({
   palette: {
     primary: {
-      //darkBlue
-      main: "#04263F",
+      // darkBlue
+      main: '#04263F',
     },
     secondary: {
-      //Brown
-      main: "#99580B",
+      // Brown
+      main: '#99580B',
     },
   },
 });
 
 function App() {
-  // const [navBackground, setNavBackground] = useState("transparent");
-  // const [username, setUsername] = useState("");
-  // const [cartCount, setCartCount] = useState(0);
+  const [navBackground, setNavBackground] = useState('transparent');
+  const [username, setUsername] = useState('');
+  const [cartCount, setCartCount] = useState(0);
   return (
     <div className="App">
       <Router>
         <ThemeProvider theme={theme}>
-          {/* <NavBar
-            username={'username'}
-            cartCount={'cartCount'}
+          <NavBar
+            username={username}
+            cartCount={cartCount}
             navBackground={navBackground}
-          /> */}
+          />
           <Switch>
             <Route exact path="/">
-              <MainHeader  />
+              <MainHeader setNavBackground={setNavBackground} />
             </Route>
-            <Route exact path="/funiture"><ProductDetails/></Route>
+            <Route exact path="/funiture"><ProductDetails setNavBackground={setNavBackground} /></Route>
           </Switch>
         </ThemeProvider>
       </Router>
