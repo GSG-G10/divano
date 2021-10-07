b024fcf44e6b8695e0d8a7fa3996cf5b4a68babb
mohammed Rayyes <mod.rayyes@gmail.com>
2021-10-07 20:43:00 +0300

    cart functionality

--- server/controllers/pages/index.js
+++ server/controllers/pages/index.js
@@ -1,3 +1,34 @@
-const getCart = require('./cart');
+const { getItems, updateQuantity } = require('../../database/queries/cart');
 
-module.exports = { getCart };
+const getCart = async (req, res, next) => {
+  try {
+    const { id } = req.userObj;
+    const items = await getItems(id);
+    res.json(items);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateCartProduct = async (req, res, next) => {
+  try {
+    const { id: userId, qty } = req.userObj;
+    const { itemId } = req.params;
+    const items = await updateQuantity(qty, itemId, userId);
+    res.json(items);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteCartProduct = async (req, res, next) => {
+  try {
+    const { id, qty } = req.userObj;
+    const items = await updateQuantity(qty, id);
+    res.json(items);
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getCart, updateCartProduct, deleteCartProduct };
